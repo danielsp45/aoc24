bef06defbc0f2642bf695f712cdc2b9740b89d6d
danielsp45 <mail@danielpereira.xyz>
2024-12-03 16:22:24 +0000

    enhance: day 03

--- src/day03.rs
+++ src/day03.rs
@@ -1,15 +1,47 @@
 use aoc_runner_derive::aoc;
 use regex::Regex;
 
+#[derive(Debug, PartialEq)]
+enum Command {
+    Mul(i32, i32),
+    Do, 
+    Dont,
+}
+
+impl Command {
+    fn parse(input: &str) -> Vec<Command> {
+        let re = Regex::new(r"mul(\((\d\d?\d?)\,(\d\d?\d?)\))|do\(\)|don't\(\)").unwrap();
+
+        let mut vec = Vec::new();
+        for cap in re.captures_iter(input) {
+            if cap[0] == *"do()" {
+                vec.push(Command::Do);
+            } else if cap[0] == *"don't()" {
+                vec.push(Command::Dont);
+            } else {
+                let x = cap[2].parse::<i32>().unwrap();
+                let y = cap[3].parse::<i32>().unwrap();
+                vec.push(Command::Mul(x, y));
+            }
+        }
+
+        vec
+    }
+
+    fn execute(&self) -> i32 {
+        match self {
+            Command::Mul(x, y) => x * y,
+            _ => 0,
+        }
+    }
+}
+
 #[aoc(day3, part1)]
 pub fn part1(input: &str) -> i32 {
-    let re = Regex::new(r"mul(\((\d\d?\d?)\,(\d\d?\d?)\))").unwrap();
+    let commands = Command::parse(input);
     let mut result = 0;
-    for cap in re.captures_iter(input) {
-        let x = cap[2].parse::<i32>().unwrap();
-        let y = cap[3].parse::<i32>().unwrap();
-
-        result += x * y;
+    for command in commands.iter() {
+        result += command.execute();
     }
 
     result
@@ -17,19 +49,17 @@ pub fn part1(input: &str) -> i32 {
 
 #[aoc(day3, part2)]
 pub fn part2(input: &str) -> i32 {
-    let re = Regex::new(r"mul(\((\d\d?\d?)\,(\d\d?\d?)\))|do\(\)|don't\(\)").unwrap();
+    let commands = Command::parse(input);
+
     let mut result = 0;
     let mut state = true;
-    for cap in re.captures_iter(input) {
-        if cap [0] == *"do()" {
+    for command in commands {
+        if command == Command::Do {
             state = true;
-        } else if cap[0] == *"don't()" {
+        } else if command == Command::Dont {
             state = false;
         } else if state {
-            let x = cap[2].parse::<i32>().unwrap();
-            let y = cap[3].parse::<i32>().unwrap();
-
-            result += x * y;
+            result += command.execute();
         }
     }
 
